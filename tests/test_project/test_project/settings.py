c27362a6b78edc58f69ca1b6b586b4643dcf9ca1
Rostyslav Bryzgunov <kottenator@gmail.com>
2016-10-17 16:14:16 -0400

    Fixes for Django 1.10

--- tests/test_project/test_project/settings.py
+++ tests/test_project/test_project/settings.py
@@ -18,10 +18,25 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder'
 )
-MIDDLEWARE_CLASSES = ()
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.template.context_processors.static',
-)
+# Django < 1.8
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.template.context_processors.static'
+]
+# Django >= 1.8
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.static'
+        ]
+    }
+}]
+# Django < 1.10
+MIDDLEWARE_CLASSES = []
+# Django >= 1.10
+MIDDLEWARE = []
+# django-compressor settings
 COMPRESS_ROOT = os.path.join(BASE_DIR, 'compressor')
 COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'compressor_toolkit.precompilers.SCSSCompiler'),
